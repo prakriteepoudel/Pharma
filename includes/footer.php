6b385cf24cb3c9b2116c97b84d541691bb9b4e72
Sanam Adhikari <105903734+Sanamadk07@users.noreply.github.com>
2023-08-06 20:44:37 +0545

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,82 @@
+<div class="site-section bg-secondary bg-image" style="background-image: url('images/bg_2.jpg');">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="about.php" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_1.jpg');">
+              <div class="banner-1-inner align-self-center">
+                <h2>Pharma Products</h2>
+                <p>At MEDCARE pharmacy, we ensure that you get high-quality life-saving medicines are delivered to you on time.
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="about.php" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_2.jpg');">
+              <div class="banner-1-inner ml-auto  align-self-center">
+                <h2>Rated by Experts</h2>
+                <p>MEDCARE pharmacy continues a legacy of 20 years of success in the pharmaceutical industry.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+
+                <div class="block-7">
+                    <h3 class="footer-heading mb-4">About Us</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste deserunt similique nihil aperiam reprehenderit ipsa laudantium, recusandae aut, quasi veritatis, velit maxime incidunt nam nesciunt adipisci beatae esse. Molestiae, ratione!. </p>
+                </div>
+
+            </div>
+            <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
+                <h3 class="footer-heading mb-4">Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Supplements</a></li>
+                    <li><a href="#">Vitamins</a></li>
+                    <li><a href="#">Diet &amp; Nutrition</a></li>
+                    <li><a href="#">Tea &amp; Coffee</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="block-5 mb-5">
+                    <h3 class="footer-heading mb-4">Contact Info</h3>
+                    <ul class="list-unstyled">
+                        <li class="address">NO.18, 2ND FLOOR,
+                            Hulak Bhawan, MAHENDRAPOOL,
+                            POKHARA Ward no.9
+                            KASKI, GANDAKI PROVINCE, NEPAL - 33700</li>
+                        <li class="phone"><a href="tel://+9779867303803">+977 986 7303 803</a></li>
+                        <li class="email">email@gmail.com</li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <p>
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved | This website was made
+                  by <a href="https://sanamadhikari07.com.np/" target="_blank" class="text-primary">MR. SANAM ADHIKARI</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/main.js"></script>
